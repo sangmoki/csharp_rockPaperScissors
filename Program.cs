723db65aeb326f2604423d06cb8e1f6d57a55e80
sangmoki <yoochang4176@naver.com>
2024-07-09 13:23:27 +0900

    Feat: "기존 가위바위보 계산 로직 (하드코딩) -> if 조건을 통하여 코드 수 절감"

--- Program.cs
+++ Program.cs
@@ -10,9 +10,12 @@ namespace RockPaperScissors
             // 0: 가위, 1: 바위, 2: 보
             int aiChoice = random.Next(0, 3);
 
+            Console.WriteLine("0~2 사이의 값을 입력해주세요. \n 0: 가위, 1: 바위, 2: 보");
+
             // 콘솔에 숫자 입력 -> 변수를 숫자로 넘김
             int myChoice = Convert.ToInt32(Console.ReadLine());
 
+            // 내 입력 값
             switch(myChoice) 
             {
                 case 0:
@@ -26,6 +29,7 @@ namespace RockPaperScissors
                     break;
             }
 
+            // 컴퓨터 입력 값
             switch(aiChoice)
             {
                 case 0:
@@ -41,7 +45,7 @@ namespace RockPaperScissors
 
             // 결과 -> 기본적인 상황
             // 내가 가위를 선택한 경우
-            if (myChoice == 0) {
+/*            if (myChoice == 0) {
                 // 컴퓨터가 가위를 선택한 경우
                 if (aiChoice == 0) {
                     Console.WriteLine("무승부 입니다.");
@@ -76,6 +80,24 @@ namespace RockPaperScissors
                 } else {
                     Console.WriteLine("무승부입니다.");
                 }
+            }*/
+
+            // 결과 -> 간단한 방법(클린코드)
+            if (myChoice == aiChoice)
+            {
+                Console.WriteLine("무승부입니다.");
+            } else if (myChoice == 0 && aiChoice == 2)
+            {
+                Console.WriteLine("당신의 승리입니다.");
+            } else if (myChoice == 1 && aiChoice == 0)
+            {
+                Console.WriteLine("당신의 승리입니다.");
+            } else if (myChoice == 2 && aiChoice == 1)
+            {
+                Console.WriteLine("당신의 승리입니다.");
+            } else
+            {
+                Console.WriteLine("당신의 패배입니다.");
             }
         }
     }
